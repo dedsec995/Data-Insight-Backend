25e0b8f2706bf0febe27b256fde9309b34de21ad
dedsec995 <amitluhar49@gmail.com>
2024-10-13 00:59:23 -0400

    Updated playground

---
+++ playground/suggestions.py
@@ -0,0 +1,50 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableParallel, RunnableLambda
+import pandas as pd
+
+file_path = "car_kick.csv"  # MA Sample
+
+
+def create_df(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+
+def suggestions(model, df):
+
+    num_rows = df.shape[0]
+    num_columns = df.shape[1]
+    first_five = df.head().to_string(index=False)
+    df_string = df.to_string(index=False)
+
+    messages = [
+        ("system", "You are a data analyst who helps get data insight."),
+        (
+            "human",
+            "Given the csv. There are {num_columns} number of columns and the {num_rows} number of rows. The file path is {file_path}. The data of csv - {df_string} \n.The target columns is the last column. Write python script to balance the data using smote. Give the entire script together and update in the original csv",
+        ),
+    ]
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    chain = (
+        prompt_template
+        | model
+        | StrOutputParser()
+    )
+    result = chain.invoke(
+        {
+            "num_columns": num_columns,
+            "num_rows": num_rows,
+            "file_path": file_path,
+            "first_five": first_five,
+            "df_string": "df_string",
+        }
+    )
+    print(result)
+
+
+df = create_df(file_path)
+model = OllamaLLM(model="mistral-nemo")
+
+suggestions(model, df)
