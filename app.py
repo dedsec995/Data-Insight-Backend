64f1cbd1f60ed0df01ad226a22630757be70b684
dedsec995 <amitluhar49@gmail.com>
2024-10-14 19:25:40 -0400

    Renamed the file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import os, time, random, uuid, base64
 from io import BytesIO
-from chains import combined_visualizations
+from graphs import combined_visualizations
 
 app = Flask(__name__)
 app.secret_key = "mySecret"
